d793bcb82dde29b6778a6caca362a39ffa9edbc1
Han <szuhanchang@gmail.com>
2011-05-17 23:11:00 +0800

    Fixed swipe to go to previous and next pages feature

--- src/com/szuhanchang/friar/Friar.java
+++ src/com/szuhanchang/friar/Friar.java
@@ -14,6 +14,8 @@ import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
+import android.view.View;
+import android.view.GestureDetector.SimpleOnGestureListener;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -24,33 +26,89 @@ public class Friar extends Activity {
 	final String MIME_TYPE = "text/html";
 	final String ENCODING = "utf-8";
 	
-	FriarWebView webView;
+	WebView webView;
 	List<String> htmlFiles;
 	int currentPage = 0;
 	int totalPages = 0;
 	
 	GestureDetector gestureDetector;
+	SimpleOnGestureListener gestureListener = new SimpleOnGestureListener() {
+		private final int SWIPE_MIN_DISTANCE = 100;
+		private final int SWIPE_MAX_DISTANCE = 350;
+		private final int SWIPE_MIN_VELOCITY = 100;
+		
+		@Override
+		public boolean onDown(MotionEvent event) {
+			return false;
+		}
+
+		@Override
+		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+			final float xDistance = Math.abs(e1.getX() - e2.getX());
+			final float yDistance = Math.abs(e1.getY() - e2.getY());
+
+			if (xDistance > this.SWIPE_MAX_DISTANCE || yDistance > this.SWIPE_MAX_DISTANCE) {
+				return false;
+			}
+
+			velocityX = Math.abs(velocityX);
+			velocityY = Math.abs(velocityY);
+
+			if (velocityX > this.SWIPE_MIN_VELOCITY && xDistance > this.SWIPE_MIN_DISTANCE) {
+				if (e1.getX() > e2.getX()) { // right to left
+					// showToast("Swipe Left");
+					if (currentPage + 1 > totalPages) {
+						showToast("This is the last page of the book.");
+					}
+					else {
+						showUrl(++currentPage);
+						return true;
+					}
+				}
+				else {
+					// showToast("Swipe Right");
+					if (currentPage - 1 < 0) {
+						showToast("This is the first page of the book.");
+					}
+					else {
+						showUrl(--currentPage);
+						return true;
+					}
+				}
+			}
+
+			return false;
+		}
+	};
 
 	/** Called when the activity is first created. */
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(final Bundle savedInstanceState) {
 		System.out.println("onCreate!");
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
 
 		htmlFiles = loadBook();
 		totalPages = htmlFiles.size();
 
-		webView = new FriarWebView(this);
-		setContentView(webView);
-
+		webView = (WebView) findViewById(R.id.webview);
 		webView.setWebViewClient(new FriarWebViewClient());
-		final WebSettings settings = webView.getSettings();
-		settings.setJavaScriptEnabled(false);
+		webView.getSettings().setJavaScriptEnabled(false);
+		
+		gestureDetector = new GestureDetector(gestureListener);
+		webView.setOnTouchListener(
+				new View.OnTouchListener() {
+		            public boolean onTouch(View wv, MotionEvent event) {
+		                gestureDetector.onTouchEvent(event);
+		                return false;
+		            }
+		        }
+		);
 
 		webView.loadUrl(BASE_URL + htmlFiles.get(currentPage));
 		System.out.println(BASE_URL + htmlFiles.get(currentPage));
 	}
-
+	
 	// Handle Android physical back button.
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
@@ -59,11 +117,6 @@ public class Friar extends Activity {
 			return true;
 		}
 		return super.onKeyDown(keyCode, event);
-	}
-	
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		return super.onTouchEvent(event);
 	}
 
 	private List<String> loadBook() {
@@ -82,92 +135,21 @@ public class Friar extends Activity {
 		return htmlFiles;
 	}
 	
-	public WebView getWebView() {
-		return webView;
-	}
-
-	class FriarWebView extends WebView {
-		Context context;
-		GestureDetector gd;
-
-		public FriarWebView(Context context) {
-			super(context);
-			this.context = context;
-			FriarGestureDetector fgd = new FriarGestureDetector();
-			gd = new GestureDetector(context, fgd);
-		}
-
-		@Override
-		public boolean onTouchEvent(MotionEvent event) {
-			return gd.onTouchEvent(event) || super.onTouchEvent(event);
-		}
+	private void showUrl(int pageNum) {
+		assert pageNum >= 0 && pageNum < totalPages;
 		
-		class FriarGestureDetector extends GestureDetector.SimpleOnGestureListener {
-			private int swipe_Min_Distance = 100;
-			private int swipe_Max_Distance = 350;
-			private int swipe_Min_Velocity = 100;
-
-			private void show_toast(final String text) {
-				Toast t = Toast.makeText(context, text, Toast.LENGTH_SHORT);
-				t.show();
-				System.out.println(text);
-			}
-			
-			private void showUrl(int pageNum) {
-				assert pageNum >= 0 && pageNum < totalPages;
-				
-				String filename = htmlFiles.get(pageNum);
-				String url = BASE_URL + filename;
-				getWebView().loadUrl(url);
-				getWebView().reload();
-				show_toast(url + ", " + currentPage);
-			}
-
-			@Override
-			public boolean onDown(MotionEvent event) {
-				return false;
-			}
-
-			@Override
-			public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-				final float xDistance = Math.abs(e1.getX() - e2.getX());
-				final float yDistance = Math.abs(e1.getY() - e2.getY());
-
-				if (xDistance > this.swipe_Max_Distance || yDistance > this.swipe_Max_Distance) {
-					return false;
-				}
-
-				velocityX = Math.abs(velocityX);
-				velocityY = Math.abs(velocityY);
-
-				if (velocityX > this.swipe_Min_Velocity && xDistance > this.swipe_Min_Distance) {
-					if (e1.getX() > e2.getX()) { // right to left
-						show_toast("Swipe Left");
-						if (currentPage + 1 > totalPages) {
-							show_toast("This is the last page of the book.");
-						}
-						else {
-							showUrl(++currentPage);
-						}
-					}
-					else {
-						show_toast("Swipe Right");
-						if (currentPage - 1 < 0) {
-							show_toast("This is the first page of the book.");
-						}
-						else {
-							showUrl(--currentPage);
-						}
-					}
-					setContentView(webView);
-					return true;
-				}
-
-				return false;
-			}
-		}
+		String filename = htmlFiles.get(pageNum);
+		String url = BASE_URL + filename;
+		webView.loadUrl(url);
+		// showToast(url + ", " + currentPage);
 	}
 
+	private void showToast(final String text) {
+		Toast t = Toast.makeText(getBaseContext(), text, Toast.LENGTH_SHORT);
+		t.show();
+		System.out.println(text);
+	}
+	
 	class FriarWebViewClient extends WebViewClient {
 		@Override
 		public void onPageStarted(WebView view, String url, Bitmap favicon) {
